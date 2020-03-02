347050e6401b928af68a565261b584281a8c7e5c
Ilya <bairon.ilya.lexy@gmail.com>
2020-03-02 23:50:21 +0400

    Реализовал вывод на экран из файла.

--- src/main/java/ru/zdadco/Runner.java
+++ src/main/java/ru/zdadco/Runner.java
@@ -1,23 +1,31 @@
 package ru.zdadco;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.List;
 import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
 
-        String filePath = "D:/notes.txt";
+        Path path = Paths.get("D:/notes.txt");
         Scanner scanner = new Scanner(System.in);
         while (true) {
             try {
                 String str = scanner.nextLine();
-                if (str.equals("\\c")){
+                if (str.equals("\\c")) {
                     break;
+                } else if (str.equals("\\v")) {
+                    List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+                    for (String line : lines) {
+                        System.out.println(line);
+                    }
                 }
-                Files.write(Paths.get(filePath), (str + "\n").getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+                Files.write(path, (str + "\n").getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
             } catch (IOException ex) {
                 System.out.println("Error!!!");
                 ex.printStackTrace();
